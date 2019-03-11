5ff44059f1308b5fc239c627b258c7013ea69fbc
Kate <kate@sicchio.com>
2019-03-11 14:00:41 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,3 @@
-{
-  var peg = require("pegjs");
-}
 
 {
   var mArissa = document.createElement("img");
@@ -8,22 +5,14 @@ mArissa.src ="images/m01.jpg";
 document.body.appendChild(mArissa);
 //document.write("work please")
 
-var dir = "images/";
-var fileextension = ".jpg";
-$.ajax({
-    //This will retrieve the contents of the folder if the folder is configured as 'browsable'
-    url: dir,
-    success: function (data) {
-        //List all .png file names in the page
-        $(data).find("a:contains(" + fileextension + ")").each(function () {
-            var filename = this.href.replace(window.location.host, "").replace("http://", "");
-            $("body").append("<img src='" + dir + filename + "'>");
-        });
-    }
-});
+start = (movement / phrase / time)
 
-}
+movement = duck / kneel / fall
 
-start = "dance" {return mArissa;}
+duck = " " {return mArissa;}
+kneel = " " {return mArissa;}
+fall = " " {return mArissa;}
 
-dance
+phrase = (retrograde / accumulation / ABBA)
+
+time = (speed / sustain / sudden)
