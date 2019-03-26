5da42f8b8936ae8053cd969a1fd3fc989a62b031
Kate <kate@sicchio.com>
2019-03-26 16:42:50 -0400

    for browser version - codemirror not in directory

---
+++ pegjs/marissaparser.js
@@ -0,0 +1,38 @@
+
+<script>
+{
+  var mArissa = document.createElement("img");
+mArissa.src ="images/m01.jpg";
+}
+
+start = (time / movement)+
+
+time = "[" loop:movement+ "]" _ {
+	var dur = 1/movement.length;
+	var results = [];
+	for (var movement of loop) {
+		result.push(movement);
+		result.push([dur, 0.25]);
+	}
+	return [loop, results];
+}
+
+movement = duck / kneel / fall / rest
+
+rest = " " { return []; }
+duck = "duck" {return mArissa;}
+kneel = "kneel" {return mArissa;}
+fall = "fall" {return mArissa;}
+
+//phrase = (retrograde / accumulation / ABBA)
+//	retrograde = reverse();
+
+_ = [ \t\n\r]*
+//space
+
+
+var peg = require("pegjs");
+
+//var parser = peg.generate("start = ('a' / 'b')+");
+
+</script>
